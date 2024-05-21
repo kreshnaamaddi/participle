0b4530c9c6d1f259c0403fa9e56b01a9002c4bee
kreshnaamaddi <kreshnaamaddi96@gmail.com>
2024-05-21 10:39:51 +0530

    Update select parser by adding joins and sub queries

--- _examples/sql/main.go
+++ _examples/sql/main.go
@@ -18,164 +18,192 @@ func (b *Boolean) Capture(values []string) error {
 }
 
 // Select based on http://www.h2database.com/html/grammar.html
-type Select struct {
-	Top        *Term             `"SELECT" ( "TOP" @@ )?`
-	Distinct   bool              `(  @"DISTINCT"`
-	All        bool              ` | @"ALL" )?`
-	Expression *SelectExpression `@@`
-	From       *From             `"FROM" @@`
-	Limit      *Expression       `( "LIMIT" @@ )?`
-	Offset     *Expression       `( "OFFSET" @@ )?`
-	GroupBy    *Expression       `( "GROUP" "BY" @@ )?`
+type QueryParser struct {
+	Pos    lexer.Position
+	Select *SelectCmd `  "SELECT" @@ `
+}
+
+type SelectCmd struct {
+	Pos           lexer.Position
+	Distinct      *Distinct         `( "DISTINCT" @@ )?`
+	All           *bool             `@("ALL")?`
+	Expression    *SelectExpression `@@ ?`
+	From          *From             `("FROM" (@@))?`
+	SetOperations *SetOperations    `@@?`
+}
+
+type SetOperations struct {
+	Pos        lexer.Position
+	Operation  *string      `@SetOpeartions`
+	All        *bool        `@("ALL")?`
+	Distinct   *bool        `@("DISTINCT")?`
+	OtherQuery *QueryParser `@@`
 }
 
 type From struct {
-	TableExpressions []*TableExpression `@@ ( "," @@ )*`
-	Where            *Expression        `( "WHERE" @@ )?`
+	Pos      lexer.Position
+	SubQuery *QueryParser       `("("? @@ ")"? )?`
+	Tables   []*TableExpression `  (@@ ( "," @@ )*)?`
+	Join     []*Joins           `(@@ ( "" @@ )*)?`
+	Where    *ConditionExpress  `( "WHERE" @@ )?`
 }
 
-type TableExpression struct {
-	Table  string        `( @Ident ( "." @Ident )*`
-	Select *Select       `  | "(" @@ ")"`
-	Values []*Expression `  | "VALUES" "(" @@ ( "," @@ )* ")")`
-	As     string        `( "AS" @Ident )?`
+type ConditionExpress struct {
+	Pos lexer.Position
+	OR  []*OrCondition ` @@ ( "OR" @@ )*`
 }
-
-type SelectExpression struct {
-	All         bool                 `  @"*"`
-	Expressions []*AliasedExpression `| @@ ( "," @@ )*`
-}
-
-type AliasedExpression struct {
-	Expression *Expression `@@`
-	As         string      `( "AS" @Ident )?`
-}
-
-type Expression struct {
-	Or []*OrCondition `@@ ( "OR" @@ )*`
-}
-
 type OrCondition struct {
-	And []*Condition `@@ ( "AND" @@ )*`
+	Pos lexer.Position
+	And []*Condition ` @@ ( "AND" @@ )*  `
 }
 
 type Condition struct {
-	Operand *ConditionOperand `  @@`
-	Not     *Condition        `| "NOT" @@`
-	Exists  *Select           `| "EXISTS" "(" @@ ")"`
-}
-
-type ConditionOperand struct {
-	Operand      *Operand      `@@`
-	ConditionRHS *ConditionRHS `@@?`
-}
-
-type ConditionRHS struct {
-	Compare *Compare `  @@`
-	Is      *Is      `| "IS" @@`
-	Between *Between `| "BETWEEN" @@`
-	In      *In      `| "IN" "(" @@ ")"`
-	Like    *Like    `| "LIKE" @@`
-}
-
-type Compare struct {
-	Operator string         `@( "<>" | "<=" | ">=" | "=" | "<" | ">" | "!=" )`
-	Operand  *Operand       `(  @@`
-	Select   *CompareSelect ` | @@ )`
-}
-
-type CompareSelect struct {
-	All    bool    `(  @"ALL"`
-	Any    bool    ` | @"ANY"`
-	Some   bool    ` | @"SOME" )`
-	Select *Select `"(" @@ ")"`
-}
-
-type Like struct {
-	Not     bool     `[ @"NOT" ]`
-	Operand *Operand `@@`
-}
-
-type Is struct {
-	Not          bool     `[ @"NOT" ]`
-	Null         bool     `( @"NULL"`
-	DistinctFrom *Operand `  | "DISTINCT" "FROM" @@ )`
-}
-
-type Between struct {
-	Start *Operand `@@`
-	End   *Operand `"AND" @@`
-}
-
-type In struct {
-	Select      *Select       `  @@`
-	Expressions []*Expression `| @@ ( "," @@ )*`
-}
-
-type Operand struct {
-	Summand []*Summand `@@ ( "|" "|" @@ )*`
-}
-
-type Summand struct {
-	LHS *Factor `@@`
-	Op  string  `[ @("+" | "-")`
-	RHS *Factor `  @@ ]`
-}
-
-type Factor struct {
-	LHS *Term  `@@`
-	Op  string `( @("*" | "/" | "%")`
-	RHS *Term  `  @@ )?`
-}
-
-type Term struct {
-	Select        *Select     `  @@`
-	Value         *Value      `| @@`
-	SymbolRef     *SymbolRef  `| @@`
-	SubExpression *Expression `| "(" @@ ")"`
-}
-
-type SymbolRef struct {
-	Symbol     string        `@Ident @( "." Ident )*`
-	Parameters []*Expression `( "(" @@ ( "," @@ )* ")" )?`
+	Pos                 lexer.Position
+	Not                 *bool        `(@"NOT")?`
+	Openbracket         *string      `"("?`
+	LHSFieldTableName   *string      `( @Ident ( ".")`
+	LHSFieldName        *string      ` @Ident `
+	Value               *Expression  `| "("? @@ ")"? )?`
+	ValueIndex          *int         `("[" @Int "]")?`
+	LHSTypecaste        *string      `("::" @Ident )?`
+	Operator            *string      `(@( "<>" | "<=" | ">=" | "=" | "<" | ">" | "!=" | "<@"| "@>"| "&&"  )`
+	Like                *bool        ` | @ "LIKE" `
+	NotLike             *bool        ` | @ "NOT" "LIKE" `
+	ILike               *bool        ` | @ "ILIKE" `
+	NotILike            *bool        ` | @ "NOT" "ILIKE" `
+	SimilarTo           *bool        ` | @ "SIMILAR" "TO" `
+	NotSimilarTo        *bool        ` | @ "NOT" "SIMILAR" "TO" `
+	Is                  *bool        `|  @ "IS" `
+	Between             []*Value     `| "BETWEEN" ("("? @@ ( "AND"   @@ ) ")"?)`
+	NotBetween          []*Value     `| "NOT" "BETWEEN" ("("? @@ ( "AND"   @@ ) ")"?)`
+	In                  *bool        `| @ "IN" `
+	NotIn               *bool        `| @ "NOT" "IN" )?`
+	RHSFieldTableName   *string      `( @Ident ( ".")`
+	RHSFieldName        *string      ` @Ident )?`
+	Val                 []*Value     `("("? @@ ( ","   @@ )* ")"?)?`
+	RHSTypecaste        *string      `("::" @Ident)?`
+	AliasColumn         *string      `( "AS" @Ident )?`
+	SubOperator         *string      `( (@SubQueryOperators)? `
+	SubQuery            *QueryParser `"("? @@ ")"? )?`
+	StuffUntilSemicolon []*string    ` (@Ignore ( @!(";") )* (";")?)?`
+	Closebracket        *string      `")"?`
 }
 
 type Value struct {
-	Wildcard bool     `(  @"*"`
-	Number   *float64 ` | @Number`
-	String   *string  ` | @String`
-	Boolean  *Boolean ` | @("TRUE" | "FALSE")`
-	Null     bool     ` | @"NULL"`
-	Array    *Array   ` | @@ )`
+	Pos          lexer.Position
+	ArrayLHSCat  *int     `( @Int "||" )?`
+	Array        []*Value ` "ARRAY" "[" @@ ("," @@)* "]"`
+	ArrayRHSCat  *int     `("||" @Int )?`
+	Int          *int     `| @Int`
+	Float        *float64 `| @Float`
+	String       *string  ` | @String`
+	Null         *bool    ` | @"NULL" `
+	Variable     *string  ` | @Ident `
+	IndexofArray *int     `("[" @Int "]")?`
+	NotNull      *bool    ` | @ "Not" "NULL" `
 }
 
-type Array struct {
-	Expressions []*Expression `"(" @@ ( "," @@ )* ")"`
+type TableExpression struct {
+	Pos                 lexer.Position
+	SchemaName          *string     `( @Ident (".") `
+	SchemaTable         *string     `@Ident`
+	Function            *string     `| @Functions`
+	Functionparams      *Expression ` "(" @@ ")" `
+	TableName           *string     `| @Ident) `
+	AliasFunctionName   *string     `( "AS" @Ident  `
+	AliasFunctionKeys   []*Value    `"(" @@ ( "," @@ )* ")" )?`
+	AliasTableName      *string     `( ("AS")? @Ident )?`
+	StuffUntilSemicolon []*string   ` (@Ignore ( @!(";") )* (";")?)?`
+}
+
+type Distinct struct {
+	Pos       lexer.Position
+	WithOn    *WithOn             `@@`
+	WithoutOn []*SelectExpression `| @@`
+}
+type WithOn struct {
+	Pos        lexer.Position
+	On         *bool             `@("ON") `
+	Columns    []string          ` "(" @Ident ( "," @Ident )* ")" `
+	Expression *SelectExpression `@@ `
+}
+
+type SelectExpression struct {
+	Pos                 lexer.Position
+	Columns             []*AsExpression `"("? @@ ( "," @@ )* ")"?`
+	StuffUntilSemicolon []*string       ` (@Ignore ( @!(";") )* (";")?)?`
+}
+
+type Aggregate struct {
+	Pos       lexer.Position
+	Aggregate *string `@Aggregate`
+	Val       *string `("(" @Ident ")"`
+	ValAll    *bool   `|"(" @"*"  ")")`
+}
+type AsExpression struct {
+	Pos            lexer.Position
+	FieldTableName *string       `(@Ident ( "." ) `
+	ShortFiledName *string       `@Ident  `
+	IndexofArray   *int          `("[" @Int "]")?`
+	AsteriskExp    *bool         `| @"*"`
+	Aggregate      *Aggregate    `| @@ `
+	Function       *string       `| @Functions`
+	Functionparams []*Expression ` "(" @@ ( "," @@ )* ")"  `
+	Expression     *Expression   `| "("? @@ ")"? `
+	SubQuery       *QueryParser  `| "("? @@ ")"? )`
+	As             *string       `( ("AS")? (@Functions|@Ident))?`
+}
+
+type Expression struct {
+	Pos        lexer.Position
+	Value      *Value      `@@`
+	Operator   *string     `(@("->>" | "->" | "#>>" | "#>" )`
+	JsonColumn *Expression `@@ )?`
+}
+
+// JOINS
+type Joins struct {
+	Pos       lexer.Position
+	JoinsType *string          `@Joins`
+	Table     *TableExpression `@@`
+	On        *bool            `@ "ON"`
+	LHSTable  *string          `(@Ident ( "."  )`
+	LHSField  *string          `@Ident)?`
+	Operator  *string          `@( "<>" | "<=" | ">=" | "=" | "<" | ">" | "!=" | "<@"| "@>"| "&&"  ) ?`
+	RHSTable  *string          `(@Ident ( ".")`
+	RHSField  *string          `@Ident)?`
+	Column    *Value           `(@@)?`
+	// OtherJoin  *Joins           `(@@)?`
+	Expression *Condition `(@@)?`
 }
 
 var (
-	cli struct {
-		SQL string `arg:"" required:"" help:"SQL to parse."`
-	}
-
 	sqlLexer = lexer.MustSimple([]lexer.SimpleRule{
-		{`Keyword`, `(?i)\b(SELECT|FROM|TOP|DISTINCT|ALL|WHERE|GROUP|BY|HAVING|UNION|MINUS|EXCEPT|INTERSECT|ORDER|LIMIT|OFFSET|TRUE|FALSE|NULL|IS|NOT|ANY|SOME|BETWEEN|AND|OR|LIKE|AS|IN)\b`},
+		{`Keyword`, `(?i)\b(SELECT|DISTINCT|AS|FROM|ON|WHERE|NOT|AND|OR|BETWEEN|ILIKE|SIMILAR|TO|BETWEEN|ARRAY|IS)\b`},
+		{`SubQueryOperators`, `(?i)\b(ANY|ALL|EXISTS)\b`},
+		{`Functions`, `(?i)\b(JSONB_ARRAY_ELEMENTS|JSONB_EACH|TRIM_ARRAY|ARRAY_CAT|ARRAY_APPEND|ARRAY_LENGTH|ARRAY_DIMS|ARRAY_NDIMS|ARRAY_LOWER|ARRAY_UPPER|ARRAY_TO_STRING|CARDINALITY|ARRAY_PREPEND|ARRAY_REPLACE|ARRAY_REMOVE|ARRAY_POSITION|ARRAY_POSITIONS|ARRAY_FILL)\b`},
+		{`Ignore`, `(?i)\b(GROUP BY|HAVING|ORDER BY|LIMIT|FETCH|OFFSET|OVER)\b`},
+		{`SetOpeartions`, `(?i)\b(UNION|INTERSECT|EXCEPT)\b`},
+		{`Aggregate`, `(?i)\b(SUM|MIN|MAX|AVG|COUNT)\b`},
+		{`Joins`, `(?i)\b(JOIN|INNER JOIN|FULL OUTER JOIN|LEFT JOIN|RIGHT JOIN|CROSS JOIN)\b`},
 		{`Ident`, `[a-zA-Z_][a-zA-Z0-9_]*`},
-		{`Number`, `[-+]?\d*\.?\d+([eE][-+]?\d+)?`},
+		{`Float`, `-?\d*\.\d+`},
+		{`Int`, `[0-9]+`},
+		{`Operators`, `<>|!=|#>>|#>|<@|@>|->>|->|<=|>=|::|&&|[-+*/%,.()=<>]|\|\|`},
 		{`String`, `'[^']*'|"[^"]*"`},
-		{`Operators`, `<>|!=|<=|>=|[-+*/%,.()=<>]`},
-		{"whitespace", `\s+`},
+		{"Comment", `(?:#|--)[^\n]*\n?`},
+		{"Semicolon", `(?:#|;)[^\n]*\n?`},
+		{"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`},
+		{"Whitespace", `\s+`},
 	})
-	parser = participle.MustBuild[Select](
+	parser = participle.MustBuild(&QueryParser{},
 		participle.Lexer(sqlLexer),
+		participle.CaseInsensitive("Keyword", "Ignore", "Functions", "Ident", "Joins", "Aggregate", "SetOpeartions", "SubQueryOperators"),
+		participle.Elide("Comment", "Semicolon", "Whitespace"),
+		participle.UseLookahead(2),
 		participle.Unquote("String"),
-		participle.CaseInsensitive("Keyword"),
-		// participle.Elide("Comment"),
-		// Need to solve left recursion detection first, if possible.
-		// participle.UseLookahead(),
 	)
 )
-
 func main() {
 	ctx := kong.Parse(&cli)
 	sql, err := parser.ParseString("", cli.SQL)
